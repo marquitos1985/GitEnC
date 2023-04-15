379b2e15eb8dda5eb81dca0381ed30acb5104b4f
marquitos1985 <marquitos1985@hotmail.com>
2023-04-15 09:45:02 -0300

    1er commit en ramaMensaje

--- main.c
+++ main.c
@@ -8,7 +8,10 @@ int main()
     printf("Hello world!\n");
     printf("Hello world!\n");
     printf("Hello world!\n");
-    int numero = 30;
+    int numero = 10;
     int flag = 2;
     return 0;
+}
+void mensaje(){
+    printf("Mensajeee..... hola");
 }
